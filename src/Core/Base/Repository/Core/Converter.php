5a10c81b4011f36ad3349bc9f21c46c9f4a3b56a
roee hershko <hershko765@gmail.com>
2014-05-10 18:52:36 +0300

    Added converter abstract functions

--- src/Core/Base/Repository/Core/Converter.php
+++ src/Core/Base/Repository/Core/Converter.php
@@ -11,4 +11,24 @@
  */
 class Converter {
 
-} // End Converter 
+	public function String($val)
+	{
+		return ((string) $val).'';
+	}
+
+	public function Integer($val)
+	{
+		return (int) $val + 0;
+	}
+
+	public function JSON($val)
+	{
+		return is_array($val) ? json_encode($val) : '['.$val.']';
+	}
+
+	public function Bool($val)
+	{
+		return $val == 'false' ? FALSE : (bool) $val;
+	}
+
+} // End Converter
