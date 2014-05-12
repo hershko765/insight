dacf849e865dfdaf8351e6250e636e751a1c606a
roee hershko <hershko765@gmail.com>
2014-05-12 08:08:40 +0300

    Rest API (WIP)

---
+++ src/App/ManagerBundle/Controller/API/AddonController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\ManagerBundle\Controller\API;
+
+use App\SourceBundle\Base;
+
+
+use App\SourceBundle\Helpers\Arr;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use FOS\RestBundle\Controller\Annotations;
+
+// Annotations dependency
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Util\Codes;
+use FOS\RestBundle\View\View;
+use FOS\RestBundle\Request\ParamFetcherInterface;
+use Symfony\Component\Form\FormTypeInterface;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+
+class AddonController extends Base\Controller {
+
+	/**
+	 * Get single Addon,
+	 *
+	 * @ApiDoc(
+	 *   resource = true,
+	 *   description = "Gets a Page for a given id",
+	 *   output = "Acme\BlogBundle\Entity\Page",
+	 *   statusCodes = {
+	 *     200 = "Returned when successful",
+	 *     404 = "Returned when the page is not found"
+	 *   }
+	 * )
+	 *
+	 * @Annotations\View(templateVar="addon")
+	 *
+	 * @param int     $id      the page id
+	 *
+	 * @return array
+	 *
+	 * @throws NotFoundHttpException when page not exist
+	 */
+	public function getAddonAction($id)
+	{
+		$handler = $this->getHandler('addon', 'get');
+
+		return $handler->setData($id)->execute();
+	}
+
+	/**
+	 * Get single Addon,
+	 *
+	 * @ApiDoc(
+	 *   resource = true,
+	 *   description = "Get an addon by given ID",
+	 *   filters={
+	 *      {"name"="addon", "dataType"="string", "description"="search addon by name"},
+	 *      {"name"="limit", "dataType"="integer"},
+	 *      {"name"="offset", "dataType"="integer"},
+	 *      {"name"="order", "dataType"="string", "description"="order by column"}
+	 *   },
+	 *   output = "App\ManagerBundle\Entities\Model\Addon",
+	 *   statusCodes = {
+	 *     200 = "Returned when successful",
+	 *     404 = "Returned when the addon is not found"
+	 *   }
+	 * )
+	 *
+	 * @Annotations\View(templateVar="addon")
+	 *
+	 * @param Request $request the request object
+	 * @param int     $id      the page id
+	 *
+	 * @return array
+	 *
+	 * @throws NotFoundHttpException when page not exist
+	 */
+	public function getAddonsAction(Request $request)
+	{
+		$query    = $request->query->all();
+		$paging   = Arr::extract($query, [ 'limit', 'offset', 'page', 'order' ]);
+		$filters  = Arr::extract($query, [ 'addon' ]);
+		$settings = Arr::extract($query, [ 'select' ]);
+
+		$handler  = $this->getHandler('addon', 'collect');
+		return $handler->setData($filters, $paging, $settings)->execute();
+	}
+
+	public function getRoeeAction() {}
+
+}
--- src/App/ManagerBundle/Controller/DefaultController.php
+++ src/App/ManagerBundle/Controller/DefaultController.php
@@ -3,8 +3,22 @@
 namespace App\ManagerBundle\Controller;
 
 use App\SourceBundle\Base;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use FOS\RestBundle\Controller\Annotations;
+
+// Annotation Dependency
+use FOS\RestBundle\Request\ParamFetcherInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Form\FormTypeInterface;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Util\Codes;
+use FOS\RestBundle\View\View;
+
+
 
 class DefaultController extends Base\Controller
 {
